e17c8088ed8a0733ba92092f59aa224fd8ae4b9a
fitgit <fitgit@users.noreply.github.com>
2015-07-19 14:13:12 -0400

    send items back

--- views/js/moverPizza.js
+++ views/js/moverPizza.js
@@ -5,7 +5,7 @@ onmessage= function (event) {
   var scrollTop=event.scrollTop;
   console.log("items=" + items.length + "scrollTop=" + event.scrollTop);
   for (var i = 0; i < items.length; i++) {
-    phase = Math.sin((IscrollTop / 1250) + (i % 5));
+    phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
   console.log("Posting items to main");
