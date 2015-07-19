70f49c9d36c3e4c942543e05c3d0333656a1b42b
fitgit <fitgit@users.noreply.github.com>
2015-07-19 14:08:08 -0400

    added event json.

--- views/js/moverPizza.js
+++ views/js/moverPizza.js
@@ -1,7 +1,9 @@
-onmessage= function (items,scrollTop) {
+onmessage= function (event) {
   console.log("Begin moverPizza function");
   var phase;
-  var IscrollTop=parseInt(scrollTop);
+  var items=event.items;
+  var scrollTop=event.scrollTop;
+  console.log("items=" + items.length + "scrollTop=" + event.scrollTop);
   for (var i = 0; i < items.length; i++) {
     phase = Math.sin((IscrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
