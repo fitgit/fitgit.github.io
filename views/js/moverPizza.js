ec04d17a26743ceb4c815a7dbcd68b7b530b145e
fitgit <fitgit@users.noreply.github.com>
2015-07-19 15:50:16 -0400

    Update moverPizza.js

--- views/js/moverPizza.js
+++ views/js/moverPizza.js
@@ -1,6 +1,9 @@
 onmessage= function (event) {
   console.log("Begin moverPizza function");
-  var items = document.querySelectorAll('.mover');
+  /*
+  var items = document.querySelectorAll('.mover');*/
+  var items=JSON.parse(event.data.items);
+  console.log("item array=" + item.length);
   var phase;
   var scrollTop=event.data.scrollTop;
   console.log("items=" + items.length + "scrollTop=" + event.scrollTop);
