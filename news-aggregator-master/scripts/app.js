8e30b253a3f47d49725f76edaf4cfd14a9ce69b4
fitgit <fitgit@users.noreply.github.com>
2015-07-29 14:44:48 -0400

    Changed querySelectorAll to getElementsByClassName

--- news-aggregator-master/scripts/app.js
+++ news-aggregator-master/scripts/app.js
@@ -259,15 +259,15 @@ APP.Main = (function() {
    */
   function colorizeAndScaleStories() {
 
-    var storyElements = document.querySelectorAll('.story');
-
+    //SP:var storyElements = document.querySelectorAll('.story');
+    var storyElements = document.getElementsByClassName('story');
     // It does seem awfully broad to change all the
     // colors every time!
     for (var s = 0; s < storyElements.length; s++) {
 
       var story = storyElements[s];
-      var score = story.querySelector('.story__score');
-      var title = story.querySelector('.story__title');
+      var score = story.getElementsByClassName('story__score');
+      var title = story.getElementsByClassName('story__title');
 
       // Base the scale on the y position of the score.
       var height = main.offsetHeight;
