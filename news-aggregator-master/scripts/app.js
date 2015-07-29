d72dd88bc85061f3f6b1908842e6a108acefd6fc
fitgit <fitgit@users.noreply.github.com>
2015-07-29 13:46:53 -0400

    Update app.js

--- news-aggregator-master/scripts/app.js
+++ news-aggregator-master/scripts/app.js
@@ -190,8 +190,10 @@ APP.Main = (function() {
       left += (0 - storyDetailsPosition.left) * 0.1;
 
       // Set up the next bit of the animation if there is more to do.
-      if (Math.abs(left) > 0.5)
-        setTimeout(animate, 4);
+      if (Math.abs(left) > 0.5){
+        // SP:setTimeout(animate, 4);
+        requestAnimationFrame(animate);
+      }  
       else
         left = 0;
 
@@ -204,7 +206,8 @@ APP.Main = (function() {
     // every few milliseconds. That's going to keep
     // it all tight. Or maybe we're doing visual changes
     // and they should be in a requestAnimationFrame
-    setTimeout(animate, 4);
+    // SP:setTimeout(animate, 4);
+    requestAnimationFrame(animate);
   }
 
   function hideStory(id) {
@@ -230,7 +233,8 @@ APP.Main = (function() {
 
       // Set up the next bit of the animation if there is more to do.
       if (Math.abs(left - target) > 0.5) {
-        setTimeout(animate, 4);
+        // SP:setTimeout(animate, 4);
+        requestAnimationFrame(animate);
       } else {
         left = target;
         inDetails = false;
@@ -245,7 +249,8 @@ APP.Main = (function() {
     // every few milliseconds. That's going to keep
     // it all tight. Or maybe we're doing visual changes
     // and they should be in a requestAnimationFrame
-    setTimeout(animate, 4);
+    // SP:setTimeout(animate, 4);
+    requestAnimationFrame(animate);
   }
 
   /**
